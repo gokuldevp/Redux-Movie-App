aa69c332d5ea3f0e50724ec08f6dc6eea771309a
Gokul Dev P <gokuldev.p123@gmail.com>
2023-12-10 11:32:58 +0530

    Creating a store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
 import './styles/index.css';
 import { App } from './components';
+import movies from './Reducers';
 
 
+
+const store = createStore(movies);
+console.log('Store:',store);
+console.log('Store:',store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
